39be00611392a49907a1d3e11dbb0b4dd3430a6d
SkyOps Solutions <devops.solutions.247@gmail.com>
2026-02-09 15:32:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'youtube',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'bgarg',
+    author_email= 'bgarg63@gmail.com',
     packages= find_packages(),
     install_requires = []
 
